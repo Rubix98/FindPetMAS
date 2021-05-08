e71a46103c90182e513e904e9f0a3af910d3d518
Sebastian Krzosek <Sebastian.krzosek1998@gmail.com>
2021-05-08 17:55:22 +0200

    Test post object

--- FindPetApi-master/index.test.js
+++ FindPetApi-master/index.test.js
@@ -8,6 +8,16 @@ const userObject = {
   punkty: 0,
 };
 
+const postObject = {
+  idPosty: 2,
+  treść: "Znaleziono zwierze",
+  id_komentarz: null,
+  Zwierzęta_idZwierzecia: 3,
+  Użytkownicy_idUżytkownik: 1,
+  data_zgloszenia: "2021-03-21T19:00:00.000Z",
+  data_time: "2021-03-21T10:00:00.000Z",
+};
+
 const setUser = (val) => {
   if (
     val.idUżytkownik &&
@@ -35,6 +45,37 @@ const setUser = (val) => {
   }
 };
 
+const setPost = (val) => {
+  if (
+    val.idPosty &&
+    val.treść &&
+    val.hasOwnProperty("id_komentarz") &&
+    val.Zwierzęta_idZwierzecia &&
+    val.Użytkownicy_idUżytkownik &&
+    val.data_zgloszenia &&
+    val.data_time
+  ) {
+    x = new Date(val.data_zgloszenia);
+    y = new Date(val.data_time);
+    if (
+      val.idPosty >= 0 &&
+      val.treść.length > 6 &&
+      val.Zwierzęta_idZwierzecia >= 0 &&
+      val.Użytkownicy_idUżytkownik >= 0 &&
+      x.getMonth() &&
+      y.getMonth()
+    ) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
 test("user object test", () => {
   expect(setUser(userObject)).toBe(true);
+});
+
+test("post object test", () => {
+  expect(setPost(postObject)).toBe(true);
 });
